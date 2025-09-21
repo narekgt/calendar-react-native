a369a2500bbedebce56807b466ebc45dd3b52aa1
narekgt <ntutundzhian@gmail.com>
2025-09-21 17:42:55 +0200

    feat(screens): add HomeScreen combining AddTodo and CalendarView components

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import { AddTodo } from '../components/AddTodo';
+import { CalendarView } from '../components/CalendarView';
+
+export const HomeScreen: React.FC = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <AddTodo />
+      <CalendarView />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+});
