57128fc0d28424b8d4b6e06fe94abbc9dbcac9d5
narekgt <ntutundzhian@gmail.com>
2025-09-21 17:45:13 +0200

    feat(navigation): add AppNavigator with Redux store and HomeScreen

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
+import { HomeScreen } from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export const AppNavigator: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
